dc2495b840beebab15f74d2dffbef1ae39cd4972
ms-nonsense <hidden@email.com>
2023-03-18 16:34:20 +0900

    Add table schema

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS python_exercise;
+USE python_exercise;
+CREATE TABLE IF NOT EXISTS financial_data (
+    symbol          VARCHAR(20)     NOT NULL,
+    date            DATE            NOT NULL,
+    open_price      DECIMAL(8, 4)   SIGNED  NOT NULL,
+    close_price     DECIMAL(8, 4)   SIGNED  NOT NULL,
+    volume          INT             SIGNED NOT NULL,
+    PRIMARY KEY (symbol, date)
+);
